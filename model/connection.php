a5b5a8cb61347b58c2139ed99732c5c1f03be032
Luu Duc Hai <duchai621859@gmail.com>
2022-12-16 03:09:24 +0700

    Fix connection model

--- model/connection.php
+++ model/connection.php
@@ -1,23 +1,24 @@
 <?php    
     class connection {
         //Config your database here
-        private $host = 'localhost';
-        private $username = 'root';
-        private $password = '';
-        private $dbName = 'air_ticket_management';
+        private static $host = 'localhost';
+        private static $username = 'root';
+        private static $password = '';
+        private static $dbName = 'air_ticket_management';
         
         //Not use
-        public function __construct ($host, $username, $password, $dbName) {
-            $this->host = $host;
-            $this->username = $username;
-            $this->password = $password;
-            $this->dbName = $dbName;
-        }
+        // public function __construct ($host, $username, $password, $dbName) {
+        //     $this->host = $host;
+        //     $this->username = $username;
+        //     $this->password = $password;
+        //     $this->dbName = $dbName;
+        // }
 
         //The way to call connect database:
         //$conn = connection::connectToDatabase ();
-        public function connectToDatabase () {
-            $conn = new mysqli ($this->host, $this->username, $this->password, $this->dbName);
+        public static function connectToDatabase () {
+            // $conn = new mysqli (get_host(), get_username(), get_password(), get_dbName());
+            $conn = new mysqli (self::$host, self::$username, self::$password, self::$dbName);
             //Check connection status later
             // if ($conn -> connect_error) {
             //     die ("Unable to connect database");
@@ -25,36 +26,36 @@
             return $conn;
         }
 
-        public function get_host () {
-            return $this->host;
-        }
+        // public function get_host () {
+        //     return $this->host;
+        // }
 
-        public function get_username () {
-            return $this->username;
-        }
+        // public function get_username () {
+        //     return $this->username;
+        // }
 
-        public function get_password () {
-            return $this->password;
-        }
+        // public function get_password () {
+        //     return $this->password;
+        // }
 
-        public function get_dbName () {
-            return $this->dbName;
-        }
+        // public function get_dbName () {
+        //     return $this->dbName;
+        // }
 
-        public function set_host ($host) {
-            $this->host = $host;
-        }
+        // public function set_host ($host) {
+        //     $this->host = $host;
+        // }
 
-        public function set_username ($username) {
-            $this->username = $username;
-        }
+        // public function set_username ($username) {
+        //     $this->username = $username;
+        // }
 
-        public function set_password ($password) {
-            $this->password = $password;
-        }
+        // public function set_password ($password) {
+        //     $this->password = $password;
+        // }
 
-        public function set_dbName ($dbName) {
-            $this->dbName = $dbName;
-        }
+        // public function set_dbName ($dbName) {
+        //     $this->dbName = $dbName;
+        // }
     }
 ?>
