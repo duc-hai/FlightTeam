e71923af6cd6dfb9988dd474759748ba18196341
huuduc28 <nduc3281@gmail.com>
2022-12-18 21:39:30 +0700

    add flight and revenue management, ticket purchase history

---
+++ model/Flight.php
@@ -0,0 +1,169 @@
+<?php
+    class Flight{
+        public $idBooking;
+        public $seatPosition;
+        public $enntranceGate;   
+        public $ticketType;
+        public $idFlight;
+        public $airName;
+        public $flightDate;
+        public $landingDay;
+        public $priceTicket;
+
+        public $customePhoneNumber;
+        public $nameCustomer;
+        public $customerBirthday;
+        public $cccd_passport;
+        public $typeCustomer;
+        public $baggageNumber;
+        public $bookingDate;
+
+        public $Iddepartureair;
+        public $IdEndAir;
+        
+        public $idpay;
+        public $payerName;
+        public $payMethods;
+        public $paymentdate;
+
+        public $datein;
+        public $dateout;
+
+
+        public function __construct ($idBooking, $seatPosition,$enntranceGate, $idFlight,$airName,$flightDate,$landingDay,$priceTicket,
+        $customePhoneNumber,$nameCustomer,$customerBirthday,$cccd_passport,$typeCustomer,$baggageNumber,$bookingDate,$idpay,$payerName,
+        $payMethods,$paymentdate,$datein,$dateout,$Iddepartureair,$IdEndAir) {
+            
+            $this->idBooking = $idBooking;
+            $this->seatPosition = $seatPosition;
+            $this->enntranceGate = $enntranceGate;
+            $this->ticketType = $ticketType;
+            $this->idFlight = $idFlight;
+            $this->airName = $airName;
+            $this->flightDate = $flightDate;
+            $this->landingDay = $landingDay;
+            $this->priceTicket = $priceTicket;
+
+            $this->customePhoneNumber = $customePhoneNumber;
+            $this->nameCustomer = $nameCustomer;
+            $this->customerBirthday = $customerBirthday;
+            $this->cccd_passport = $cccd_passport;
+            $this->typeCustomer = $typeCustomer;
+            $this->baggageNumber = $baggageNumber ;
+            $this->bookingDate = $bookingDate;
+
+            $this->idpay = $idpay;
+            $this->payerName = $payerName;
+            $this->payMethods = $payMethods;
+            $this->paymentdate = $paymentdate;
+
+            $this->datein = $datein;
+            $this->dateount = $dateout;
+
+            $this->Iddepartureair = $Iddepartureair;
+        }
+
+        public static function getAllTicket() {
+           require_once ('connection.php');
+            $conn = connection::connectToDatabase ();
+
+            $sql = "SELECT vb.madatcho,vb.vitrighe,vb.congvao,vb.tonggiave,vb.loaive,cb.machuyenbay,cb.tenmaybay,cb.ngaydi,cb.ngayden
+            FROM vemaybay as vb, chuyenbay as cb
+            WHERE vb.machuyenbay = cb.machuyenbay";
+
+            $result = $conn -> query ($sql);
+            $data = [];
+            while ($r = $result -> fetch_assoc ()) {
+                $data[] = ["idBooking"=> $r["madatcho"],"seatPosition"=> $r["vitrighe"],"enntranceGate"=>$r["congvao"],"ticketType"=>$r['loaive'],
+                "idFlight"=>$r["machuyenbay"],"airName"=>$r["tenmaybay"],"flightDate"=>$r["ngaydi"],"landingDay"=>$r["ngayden"],"priceTicket"=>$r["tonggiave"]];
+            }
+            return $data;
+        }
+
+        public static function getHistoryTicket(){
+            require_once ('connection.php');
+            $conn = connection::connectToDatabase ();
+
+            $sql = "SELECT * FROM vemaybay,thanhtoan WHERE vemaybay.magiaodich = thanhtoan.magiaodich";
+
+            $result = $conn -> query ($sql);
+            $data = [];
+            while ($r = $result -> fetch_assoc ()) {
+                $data[] = ["idBooking"=> $r["madatcho"],"customePhoneNumber"=> $r["sodienthoaikhachdangnhap"],"nameCustomer"=> $r["hotenkhachhang"],
+                "customerBirthday"=> $r["ngaysinhkhachhang"],"cccd_passport"=>$r["cccd_passport"],"typeCustomer"=>$r['loaikhachhang'],
+                "seatPosition"=>$r["vitrighe"],"enntranceGate"=>$r["congvao"],"baggageNumber"=>$r["sohangly_tuixach"],"bookingDate"=>$r["ngaydat"],
+                "idFlight"=>$r["machuyenbay"],"idpay"=>$r["magiaodich"],"payerName"=>$r["tenkhachhang"],"payMethods"=>$r["phuongthucthanhtoan"],
+                "paymentdate"=>$r["ngaygiaodich"],"priceTicket"=>$r["tonggiave"]];
+            }
+            return $data;
+        }
+        public static function getRevenue($datein,$dateout){
+            require_once ('connection.php');
+            $conn = connection::connectToDatabase ();
+
+            $sql = "SELECT *FROM vemaybay,thanhtoan WHERE vemaybay.magiaodich = thanhtoan.magiaodich and ngaydat >= '$datein' AND ngaydat <= '$dateout'";
+
+            $result = $conn -> query ($sql);
+            $data = [];
+            while ($r = $result -> fetch_assoc ()) {
+                $data[] = ["idBooking"=> $r["madatcho"],"customePhoneNumber"=> $r["sodienthoaikhachdangnhap"],"nameCustomer"=> $r["hotenkhachhang"],
+                "customerBirthday"=> $r["ngaysinhkhachhang"],"cccd_passport"=>$r["cccd_passport"],"typeCustomer"=>$r['loaikhachhang'],
+                "seatPosition"=>$r["vitrighe"],"enntranceGate"=>$r["congvao"],"baggageNumber"=>$r["sohangly_tuixach"],"bookingDate"=>$r["ngaydat"],
+                "idFlight"=>$r["machuyenbay"],"idpay"=>$r["magiaodich"],"payerName"=>$r["tenkhachhang"],"payMethods"=>$r["phuongthucthanhtoan"],
+                "paymentdate"=>$r["ngaygiaodich"],"priceTicket"=>$r["tonggiave"],"ticketType"=>$r["loaive"]];
+            }
+            return $data;
+        }
+        public static function getInforHistory($idBooking){
+            require_once ('connection.php');
+            $conn = connection::connectToDatabase ();
+            $sql = "SELECT * FROM vemaybay,thanhtoan WHERE vemaybay.magiaodich = thanhtoan.magiaodich and madatcho = '$idBooking'";
+
+            $result = $conn -> query ($sql);
+            $data = [];
+            while ($r = $result -> fetch_assoc ()) {
+                $data[] = ["idBooking"=> $r["madatcho"],"customePhoneNumber"=> $r["sodienthoaikhachdangnhap"],"nameCustomer"=> $r["hotenkhachhang"],
+                "customerBirthday"=> $r["ngaysinhkhachhang"],"cccd_passport"=>$r["cccd_passport"],"typeCustomer"=>$r['loaikhachhang'],
+                "seatPosition"=>$r["vitrighe"],"enntranceGate"=>$r["congvao"],"baggageNumber"=>$r["sohangly_tuixach"],"bookingDate"=>$r["ngaydat"],
+                "idFlight"=>$r["machuyenbay"],"idpay"=>$r["magiaodich"],"payerName"=>$r["tenkhachhang"],"payMethods"=>$r["phuongthucthanhtoan"],
+                "paymentdate"=>$r["ngaygiaodich"],"priceTicket"=>$r["tonggiave"]];
+            }
+            return $data;
+        }
+
+        public static function getflight(){
+            require_once ('connection.php');
+            $conn = connection::connectToDatabase ();
+            $sql = "SELECT * FROM chitietchuyenbay,chuyenbay WHERE chitietchuyenbay.machuyenbay = chuyenbay.machuyenbay";
+            $result = $conn -> query ($sql);
+            $data = [];
+            while ($r = $result -> fetch_assoc ()) {
+                $data[] = ["idFlight"=> $r["machuyenbay"],"airName"=> $r["tenmaybay"],"flightDate"=>$r["ngaydi"],"landingDay"=>$r["ngayden"],"Iddepartureair"=>$r["masanbay"]];
+            }
+            return $data;
+        }
+
+        public static function addflight($idFlight,$airName,$flightDate,$landingDay,$Iddepartureair){
+            require_once ('connection.php');
+            $conn = connection::connectToDatabase ();
+            $sql = "INSERT INTO `chuyenbay`(`machuyenbay`, `tenmaybay`, `ngaydi`, `ngayden`) 
+            VALUES ('$idFlight','$airName','$flightDate','$landingDay')";
+            $result = $conn -> query ($sql);
+            $sql2 = "INSERT INTO `chitietchuyenbay`(`masanbay`, `machuyenbay`) 
+            VALUES ('$Iddepartureair','$idFlight')";
+            $result = $conn -> query ($sql2);
+        }
+
+        public static function editflight($idFlight,$airName,$flightDate,$landingDay,$Iddepartureair){
+            require_once ('connection.php');
+            $conn = connection::connectToDatabase ();
+            $sql = "UPDATE `chuyenbay` SET `tenmaybay`='$airName',`ngaydi`='$flightDate',`ngayden`='$landingDay'
+            WHERE machuyenbay = '$idFlight'";
+            $result = $conn -> query ($sql);
+            $sql2 = "UPDATE `chitietchuyenbay` SET `masanbay`='$Iddepartureair'  WHERE machuyenbay = '$idFlight'";
+            $result = $conn -> query ($sql2);
+        }
+        
+    }
+    
+?>
---
+++ view/flightManager.php
@@ -0,0 +1,251 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+    <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+    <link rel="icon" href="../assests/imgs/logo.png">    
+        <title>Document</title>
+    <style>
+        :root {
+            --main-color: white;
+        }
+        #danhmuc{
+            background-color: var(--main-color);
+            text-align: center;
+            border: 1px solid white;
+        }
+        .list-group-item{
+            background-color: white;
+        }
+        .list-group-item:hover{
+            background-color:rgb(213, 184, 184);
+        }
+        body{
+            background-color:white;
+        }     
+        #addticket{
+            margin-top: 1%;
+            margin-bottom: 1%;
+        }
+    </style>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="#">FlightTeam Air</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive"
+                aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home
+                            <span class="sr-only">(current)</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href=""></a>
+                    </li>
+                    <li class="nav-item">
+                        <a id="countItemCart" class="nav-link" href=""></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+        <div class="row">
+            <div class="col-lg-2">
+                <div id="danhmuc">
+                    <img src = "/FlightTeam/assests/imgs/logo.png" style="width:100% ;height:100% ;">
+                    <div class="list-group">
+                        <a  class='list-group-item' onclick="get_all_tickets()" >Thông tin vé máy bay</a>
+                        <a class='list-group-item' onclick="get_history_tickets()" >Lịch sử mua vé</a>
+                        <a  class='list-group-item' href="revenue.php" >doanh thu</a>
+                        <a  class='list-group-item' onclick="get_flight()" >Quản lý chuyến bay</a>
+                    </div>
+                </div>
+            </div>
+            <div  class="col-md-12 col-lg-9">
+                <!-- <div id="mainRevenue"></div> -->
+                <div id="mainView"></div>
+            </div>
+        </div>
+    </div>  
+</body>
+</html> 
+<script>
+    $(document).ready(function () {
+        get_all_tickets();
+    });
+    function get_all_tickets() {
+        $.ajax({
+        url: "/FlightTeam/controller/FlightManagerControler.php?action=getAllTicket",
+        dataType: 'json',
+            success: function (data) { 
+                show_tickets (data);
+            },
+            error: function (data) {
+            }
+        })
+    }
+   
+    function show_tickets(data) {
+            let tablemain =` 
+            <div id ="tableTicket">
+                <h1 style ="margin-top: 4%">Thông tin vé máy bay</h1>
+                    <table id="list-ticket" class="table table-bordered" style ="margin-top: 2%">
+                    <thead>
+                        <tr class="header">
+                            <th>Mã đặt chỗ</th>
+                            <th>vị trí ghế</th>
+                            <th>Cổng vào</th>
+                            <th>Loại vé</th>
+                            <th>Mã chuyến bay</th>
+                            <th>Tên máy bay</th>
+                            <th>Ngày đi</th>
+                            <th>Ngày đến</th>
+                            <th>Giá vé</th>
+                        </tr>  
+                    </thead>
+                    </table>
+                </div>`
+            $('#tableHis').remove();
+            $('#tableTicket').remove();
+            $('#tableFly').remove();
+            $("#mainView").append(tablemain);
+            for (let i = 0; i < data.data.length; i++) {
+                let row = data.data[i];
+                let table = 
+                `
+                <tbody>
+                    <tr>
+                        <td>`+row.idBooking+`</td>
+                        <td>`+row.seatPosition+`</td>
+                        <td>`+row.enntranceGate+`</td>
+                        <td>`+row.ticketType+`</td>
+                        <td>`+row.idFlight+`</td>
+                        <td>`+row.airName+`</td>
+                        <td>`+row.flightDate+`</td>
+                        <td>`+row.landingDay+`</td>
+                        <td>`+row.priceTicket+`</td>
+                    </tr>
+                </tbody>
+                </table>`
+                $("#list-ticket").append(table);
+            }
+         
+        }
+
+    function get_history_tickets() {
+        $.ajax({
+        url: "/FlightTeam/controller/FlightManagerControler.php?action=gethistoryTicket",
+        dataType: 'json',
+            success: function (data) { 
+                show_history_tickets (data);
+            },
+            error: function (data) {
+            }
+        })
+        }
+
+    function show_history_tickets (data) {
+            let tablemain =`
+            <div id ="tableHis">
+            <h1 style ="margin-top: 4%">Lịch sử mua vé</h1>
+            <table id="list-history-ticket" class="table table-bordered"style ="margin-top: 2%">
+                <thead>
+                <tr>
+                    <th>Mã đặt chỗ</th>
+                    <th>Số điện thoại</th>
+                    <th>Tên khách hàng</th>
+                    <th>Mã chuyến bay</th>
+                    <th>Mã giao dịch</th>
+                    <th>Người thanh toán</th>
+                    <th>Thanh toán</th>
+                    <th>Ngày giao dịch</th>
+                    <th>Tổng tiền</th>
+                </tr>
+                </thead>
+            </div>`
+            $('#tableHis').remove();
+            $('#tableTicket').remove();
+            $('#tableFly').remove();
+            $("#mainView").append(tablemain);
+            for (let i = 0; i < data.data.length; i++) {
+                let row = data.data[i];
+                let table = 
+                `<tbody>
+                    <tr>
+                        <td>`+row.idBooking+`</td>
+                        <td>`+row.customePhoneNumber+`</td>
+                        <td>`+row.nameCustomer+`</td>
+                        <td>`+row.idFlight+`</td>
+                        <td>`+row.idpay+`</td>
+                        <td>`+row.payerName+`</td>
+                        <td>`+row.payMethods+`</td>
+                        <td>`+row.paymentdate+`</td>
+                        <td>`+row.priceTicket+`</td>
+                    </tr>
+                    </tbody>
+                </table>`
+                $("#list-history-ticket").append(table);
+            }
+         
+        }
+        //Quản lý chuyến bay
+        function get_flight() {
+            $.ajax({
+            url: "/FlightTeam/controller/FlightManagerControler.php?action=getflight",
+            dataType: 'json',
+                success: function (data) { 
+                    show_flight (data);
+                },
+                error: function (data) {
+                }
+            })
+            }
+
+        function show_flight(data) {
+            let tablemain = `
+            <div id ="tableFly">
+            <h1 style ="margin-top: 4%">Quản lý chuyến bay</h1>
+                <a href="../view/addFlight.php" id="addticket" class="btn btn-danger mt-10">Thêm chuyến bay</a>
+                <table id ="list-getflight" class="table table-hover" style ="margin-top: 1%">
+                <thead>
+                <tr>
+                    <th>Mã chuyến bay</th>
+                    <th>Tên Máy bay</th>
+                    <th>Ngày khởi hành</th>
+                    <th>Ngày đến</th>
+                    <th>Mã sân bay</th>
+                </tr>
+                </thead>
+                </div>`
+            $('#tableHis').remove();
+            $('#tableTicket').remove();
+            $('#tableFly').remove();
+            $("#mainView").append(tablemain);    
+            for (let i = 0; i < data.data.length; i++) {
+                let row = data.data[i];
+                let table = 
+                `<tbody>
+                    <tr>
+                        <td>`+row.idFlight+`</td>
+                        <td>`+row.airName+`</td>
+                        <td>`+row.flightDate+`</td>
+                        <td>`+row.landingDay+`</td>
+                        <td>`+row.Iddepartureair+`</td>
+                        <td><a href="../view/editFlight.php?idFlight=` +row.idFlight+`" class="btn btn-primary">Edit</a></td>
+                    </tr>
+                </body>
+                </table>`
+                $("#list-getflight").append(table);
+            }
+        }
+</script>   
