0ad2711957157959f024e214f9089736347493d0
Luu Duc Hai <duchai621859@gmail.com>
2022-12-19 09:08:09 +0700

    fix some bugs in manager

--- view/addFlight.php
+++ view/addFlight.php
@@ -46,7 +46,7 @@
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container">
-            <a class="navbar-brand" href="#">FlightTeam Air</a>
+            <a class="navbar-brand" href="../view/flightManager.php">FlightTeam Air</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive"
                 aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
